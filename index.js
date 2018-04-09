4a07cf9875cfb283940a9590a4ccec86cf32fd6d
hua1995116 <461249104@qq.com>
2018-04-09 22:45:27 +0800

    node服务

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+router.get('/', function (req, res, next) {
+  req.url = './index.html';
+  next();
+});
+app.use(router);
+
+app.all('*', function(req, res, next) {  
+  res.header("Access-Control-Allow-Origin", "*");  
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");  
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
+  res.header("X-Powered-By",' 3.2.1')  
+  res.header("Content-Type", "application/json;charset=utf-8");  
+  next();  
+});  
+
+app.get('/getInfo', function(req, res) {
+  console.log(req.url);
+  let user_ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(user_ip);
+  res.json();
+})
+
+app.use(express.static('./'));
+const server = app.listen(7788);
